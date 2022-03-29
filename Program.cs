b42da00312a8e6149dd7e170e254e67d6654c38d
Сергей <seregabur@live.ru>
2022-03-29 21:32:54 +0300

    2

--- Program.cs
+++ Program.cs
@@ -6,99 +6,77 @@ namespace Конвертер_валют
     {
         static void Main(string[] args)
         {
-            decimal dollarExchangeRate= 93.7m;
-            decimal euroExchangeRate= 102.67m;
-            decimal rubleExchangeRate = 1m;
+            decimal[] courseMoney = { 93.7m, 102.67m, 1m };
+            decimal[] userMoney = { 0, 0, 0 };
+            string[] nameMoney = { "Рубль", "Долар", "Евро" };
 
             Random randomAmountMoney = new Random();
 
             int startRandom = 202;
             int stopRandom = 100000;
 
-            decimal userDollar=randomAmountMoney.Next(startRandom,stopRandom);
-            decimal userEuro = randomAmountMoney.Next(startRandom, stopRandom);
-            decimal userRuble = randomAmountMoney.Next(startRandom, stopRandom);
+            for(int i=0;i<userMoney.Length;i++)
+            {
+                userMoney[i]=randomAmountMoney.Next(startRandom, stopRandom);
+            }
 
             bool flagStopProgram = true;
 
-            while(flagStopProgram)
+            while (flagStopProgram)
             {
                 Console.Clear();
-                Console.WriteLine("Ваш баланс\nРублей - " + userRuble + " Евро - " + userEuro + " Доллар - " + userDollar);
-                Console.WriteLine();
-                Console.WriteLine("Курс валют на сегодня \nрубль - " + rubleExchangeRate + " доллар - " + dollarExchangeRate);
-                Console.WriteLine("рубль - " + rubleExchangeRate + " евро - " + euroExchangeRate);
-                Console.Write("Вы хотите провести обмен? ДА Enter, НЕТ нажмите N ");
+                Console.WriteLine("Ваш баланс");
+
+                for (int i = 0; i < userMoney.Length; i++)
+                {
+                    Console.WriteLine(nameMoney[i]+ " - " + (i + 1)+" " + userMoney[i]);
+                }
+                Console.WriteLine("Курс валют на сегодня");
+
+                for (int i = 0; i < courseMoney.Length; i++)
+                {
+                    Console.WriteLine(nameMoney[i] + " - " + (i + 1)+" " + courseMoney[i]);
+                }
+                Console.Write("Вы хотите провести обмен? ДА нажмите Enter, НЕТ нажмите N, Для выхода наберите exit ");
                 string signalKey = Console.ReadLine();
 
-                if(signalKey=="N"||signalKey=="n"||signalKey=="Т"||signalKey=="т")
+                if (signalKey == "N" || signalKey == "n" || signalKey == "Т" || signalKey == "т")
+                {}
+                else if (signalKey == "exit") 
                 {
-                    flagStopProgram = false;
-                }    
+                    flagStopProgram = false; 
+                }
                 else
                 {
                     Console.WriteLine("Выберите валюту которую хотите обменять\nРубль нажмите - 1\nДоллар нажмите - 2\nЕвро нажмите - 3");
-                    string curencySelectionToExchange = Console.ReadLine();
+                    int castToArrayCellMassiv = 1;
+                    int curencySelectionToExchange = Convert.ToInt32(Console.ReadLine()) -castToArrayCellMassiv;
                     Console.Write("Сколько денег вы хотите обменять - ");
-                    string moneyToExchange = Console.ReadLine();
-                    decimal moneyToExchageDecimal = Convert.ToDecimal(moneyToExchange);
+                    decimal moneyToExchage = Convert.ToDecimal(Console.ReadLine());
                     Console.WriteLine("Выберите валюту которая вам нужна\nРубль нажмите - 1\nДоллар нажмите - 2\nЕвро нажмите - 3");
-                    string desiredCurency = Console.ReadLine();
+                    int desiredCurency = Convert.ToInt32(Console.ReadLine())-castToArrayCellMassiv;
 
-                    switch (curencySelectionToExchange)
+                    if(moneyToExchage<=userMoney[curencySelectionToExchange])
                     {
-                        case "1":
-                            switch(desiredCurency)
-                            {
-                                case "1":
+                        userMoney[curencySelectionToExchange] = userMoney[curencySelectionToExchange] - moneyToExchage;
+                        userMoney[desiredCurency] = moneyToExchage * courseMoney[desiredCurency];
+                        Console.WriteLine("На своем счету после обмена вы имеете");
 
-                                    break;
-                                case "2":
-                                    break;
-                                case "3":
-                                    break;
-                            }
-                            break;
-                        case "2":
-                            switch (desiredCurency)
-                            {
-                                case "1":
-                                    break;
-                                case "2":
-                                    break;
-                                case "3":
-                                    break;
-                            }
-                            break;
-                        case "3":
-                            switch (desiredCurency)
-                            {
-                                case "1":
-                                    break;
-                                case "2":
-                                    break;
-                                case "3":
-                                    break;
-                            }
-                            break;
-                        default:
-
-                            break;
-
+                        for (int i = 0; i < userMoney.Length; i++)
+                        {
+                            Console.WriteLine(nameMoney[i] + " - " + (i + 1) + " " + userMoney[i]);
+                        }
                     }
-
+                    else
+                    {
+                        Console.WriteLine("У вас не достаточно денег выберите, меньшее колличество");
+                    }
                 }
+                Console.WriteLine("Для продолжения нажмите Enter");
+                Console.ReadLine();
             }
+            
+
         }
-
-        private decimal Exchange(decimal moneyToExchange, decimal amountMoney,decimal rate)
-        {
-            decimal returnMoney = 0;
-
-
-
-            return returnMoney;
-        }
-
     }
 }
